e2ff6aff387c6400628cf4e6fb268ef55a5b7e01
Brian Connelly <bdc@bconnelly.net>
2021-11-21 16:58:22 -0800

    Uncomment tests

--- tests/testthat/test-brew_list.R
+++ tests/testthat/test-brew_list.R
@@ -48,39 +48,39 @@ test_that("brew_list returns the expected tibble", {
   expect_equal(levels(valid_result$type), c("formula", "cask"))
 })
 
-# valid_is_installed <- function(package = "pwgen",
-#                                formulae = TRUE,
-#                                casks = TRUE) {
-#   is_installed(
-#     package = package,
-#     formulae = formulae,
-#     casks = casks
-#   )
-# }
-#
-#
-# test_that("is_installed validates arguments correctly", {
-#   expect_error(valid_is_installed(package = X))
-#   expect_error(valid_is_installed(package = ""))
-#   expect_error(valid_is_installed(package = NA))
-#   expect_error(valid_is_installed(package = NULL))
-#   expect_error(valid_is_installed(package = character(0)))
-#
-#   expect_error(valid_is_installed(formulae = 1))
-#   expect_error(valid_is_installed(casks = 1))
-#   expect_error(valid_is_installed(formulae = "yes"))
-#   expect_error(valid_is_installed(casks = "nope"))
-#   expect_error(valid_is_installed(formulae = NA))
-#   expect_error(valid_is_installed(casks = NA))
-#
-#   expect_error(valid_is_installed(pkg = "pwgen"))
-# })
-#
-# test_that("is_installed returns a logical", {
-#   skip_on_os("windows")
-#   skip_if_not(has_homebrew())
-#
-#   expect_true(is.logical(valid_is_installed()))
-#   expect_true(is.logical(valid_is_installed(c("pwgen", "unixodbc"))))
-#   expect_true(is.logical(valid_is_installed(package = "fakepackage123")))
-# })
+valid_is_installed <- function(package = "pwgen",
+                               formulae = TRUE,
+                               casks = TRUE) {
+  is_installed(
+    package = package,
+    formulae = formulae,
+    casks = casks
+  )
+}
+
+
+test_that("is_installed validates arguments correctly", {
+  expect_error(valid_is_installed(package = X))
+  expect_error(valid_is_installed(package = ""))
+  expect_error(valid_is_installed(package = NA))
+  expect_error(valid_is_installed(package = NULL))
+  expect_error(valid_is_installed(package = character(0)))
+
+  expect_error(valid_is_installed(formulae = 1))
+  expect_error(valid_is_installed(casks = 1))
+  expect_error(valid_is_installed(formulae = "yes"))
+  expect_error(valid_is_installed(casks = "nope"))
+  expect_error(valid_is_installed(formulae = NA))
+  expect_error(valid_is_installed(casks = NA))
+
+  expect_error(valid_is_installed(pkg = "pwgen"))
+})
+
+test_that("is_installed returns a logical", {
+  skip_on_os("windows")
+  skip_if_not(has_homebrew())
+
+  expect_true(is.logical(valid_is_installed()))
+  expect_true(is.logical(valid_is_installed(c("pwgen", "unixodbc"))))
+  expect_true(is.logical(valid_is_installed(package = "fakepackage123")))
+})
